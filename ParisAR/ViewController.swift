0975c716b806ead4920f47b5eb9eb7dc74ee2737
Tomisin Jenrola <tomisinjenrola@gmail.com>
2020-10-12 18:52:25 -0400

    added README

--- ParisAR/ViewController.swift
+++ ParisAR/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealityKit
+import Combine
 
 class ViewController: UIViewController {
     
@@ -15,10 +16,92 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
+        arView.scene.addAnchor(anchor)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        var cards: [Entity] = []
+        for _ in 1...16 {
+            let box = MeshResource.generateBox(width:0.04, height: 0.002, depth:0.04)
+            let metalMaterial = SimpleMaterial(color: .gray, isMetallic: true)
+            let model = ModelEntity(mesh: box, materials: [metalMaterial])
+            
+            model.generateCollisionShapes(recursive: true)
+            
+            cards.append(model)
+        }
+        
+        for (index, card) in cards.enumerated() {
+            let x = Float(index % 4) - 2.5
+            let z = Float(index / 4) - 2.5
+            
+            card.position = [x*0.1, 0, z*0.1]
+            anchor.addChild(card)
+        }
+        
+        
+        let boxSize: Float = 0.7
+        let occlusionBoxMesh = MeshResource.generateBox(size: boxSize)
+        let occlusionBox = ModelEntity(mesh: occlusionBoxMesh, materials: [OcclusionMaterial()])
+        
+        occlusionBox.position.y = -boxSize/2
+        
+        anchor.addChild(occlusionBox)
+        
+        var cancellable: AnyCancellable? = nil
+        cancellable = ModelEntity.loadModelAsync(named: "01")
+            .append(ModelEntity.loadModelAsync(named: "02"))
+            .append(ModelEntity.loadModelAsync(named: "03"))
+            .append(ModelEntity.loadModelAsync(named: "04"))
+            .append(ModelEntity.loadModelAsync(named: "05"))
+            .append(ModelEntity.loadModelAsync(named: "06"))
+            .append(ModelEntity.loadModelAsync(named: "07"))
+            .append(ModelEntity.loadModelAsync(named: "08"))
+        .collect()
+            .sink(receiveCompletion: { error in
+                    print("Error: \(error)")
+                    cancellable?.cancel()
+            }, receiveValue: { entities in
+                var objects: [ModelEntity] = []
+                
+                for entity in entities {
+                    entity.setScale(SIMD3<Float>(0.002, 0.002, 0.002), relativeTo: anchor)
+                    entity.generateCollisionShapes(recursive: true)
+                    
+                    for _ in 1...2 {
+                        objects.append(entity.clone(recursive: true))
+                    }
+                }
+                objects.shuffle()
+                
+                for (index, object) in objects.enumerated() {
+                    cards[index].addChild(object)
+                    cards[index].transform.rotation = simd_quatf(angle: .pi, axis: [1, 0, 0])
+                }
+                
+                cancellable?.cancel()
+            })
+        
     }
+    
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: arView)
+        
+        if let card = arView.entity(at: tapLocation) {
+            if card.transform.rotation.angle == .pi {
+                var flipDownTransform = card.transform
+                flipDownTransform.rotation = simd_quatf(angle: 0, axis: [1, 0, 0])
+                
+                card.move(to: flipDownTransform, relativeTo: card.parent, duration: 0.25, timingFunction: .easeInOut)
+            } else {
+                var flipUpTransform = card.transform
+                flipUpTransform.rotation = simd_quatf(angle: .pi, axis: [1, 0, 0])
+                
+                card.move(to: flipUpTransform, relativeTo: card.parent, duration: 0.25, timingFunction: .easeInOut)
+                
+            }
+        }
+        
+    }
+    
 }
---
+++ README.md
@@ -0,0 +1,21 @@
+# ParisAR
+
+Playing around with AR. Paris Majhong style game
+
+Based on https://www.youtube.com/watch?v=mV7SWeMDEfg
+
+## Requirements
+
+- XCode
+- Physical device with iOS >= 13
+
+## Run
+
+- Open the project in XCode
+- Connect your device
+- Hit Cmd+R
+
+### Troubleshooting
+
+- Ensure your ApppleID is [added to XCode](https://stackoverflow.com/questions/39524148/xcode-error-code-signing-is-required-for-product-type-application-in-sdk-ios).
+- Ensure your iOS device can [run apps from untrusted developers](https://apple.stackexchange.com/a/206144).
