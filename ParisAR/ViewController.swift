d422c8a75eec38801f0e6bc15f4ca4bb296baf51
Tomisin Jenrola <tomisinjenrola@gmail.com>
2020-10-12 16:51:40 -0400

    Initial Commit

---
+++ ParisAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ParisAR
+//
+//  Created by Tomisin Jenrola on 2020-10-12.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
